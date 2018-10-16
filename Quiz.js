a41609cb20204ca3a967a0494d2d7e9285726635
ElianMelo <elianmelomorais@gmail.com>
2018-10-16 20:59:48 -0300

    Aula 4 - Quiz: Fatoriais!

--- Quiz.js
+++ Quiz.js
@@ -1,26 +1,12 @@
 /*
-Autor: Elian Melo Morais
-
-Data de Criação: 16/10/2018
-
-Objetivo: Mostrar os códigos de quiz utilizados no curso da udacity
-*/
-
-/*
- * Programming Quiz: Find my Seat (4-8)
- *
- * Write a nested for loop to print out all of the different seat combinations in the theater.
- * The first row-seat combination should be 0-0
- * The last row-seat combination will be 25-99
- *
- * Things to note:
- *  - the row and seat numbers start at 0, not 1
- *  - the highest seat number is 99, not 100
+ * Programming Quiz: Factorials (4-7)
  */
 
-// Write your code here
-for(var x = 0; x <= 25; x++){
-    for(var y = 0; y <= 99; y++){
-        console.log(x + "-" + y);
-    }
+// your code goes here
+var solution = 1;
+
+for(var x = 12; x > 0; x--){
+    solution *= x;
 }
+
+console.log(solution);
