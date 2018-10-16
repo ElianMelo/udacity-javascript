5e86cfe29d3e27f1b320c4c0ddc05ec8694bece5
ElianMelo <elianmelomorais@gmail.com>
2018-10-16 20:52:31 -0300

    Aula 3 - Quiz: Navegando pela cadeia alimentar

--- Quiz.js
+++ Quiz.js
@@ -7,35 +7,24 @@ Objetivo: Mostrar os códigos de quiz utilizados no curso da udacity
 */
 
 /*
- * Programming Quiz: What do I Wear? (3-7)
+ * Programming Quiz - Navigating the Food Chain (3-8)
  *
- * Using if/else statements, create a series of logical expressions that logs the size of a t-shirt based on the measurements of:
- *   - shirtWidth
- *   - shirtLength
- *   - shirtSleeve
+ * Use a series of ternary operator to set the category to one of the following:
+ *   - "herbivore" if an animal eats plants
+ *   - "carnivore" if an animal eats animals
+ *   - "omnivore" if an animal eats plants and animals
+ *   - undefined if an animal doesn't eat plants or animals
  *
- * Use the chart above to print out one of the following correct values:
- *   - S, M, L, XL, 2XL, or 3XL
+ * Notes
+ *   - use the variables `eatsPlants` and `eatsAnimals` in your ternary expressions
+ *   - `if` statements aren't allowed ;-)
  */
 
-// change the values of `shirtWidth`, `shirtLength`, and `shirtSleeve` to test your code
-var shirtWidth = 23;
-var shirtLength = 30;
-var shirtSleeve = 8.71;
+// change the values of `eatsPlants` and `eatsAnimals` to test your code
+var eatsPlants = false;
+var eatsAnimals = true;
 
-// Write your if/else code here
-if((shirtWidth >= 18 && shirtWidth < 20) && (shirtLength >= 28 && shirtLength < 29) && (shirtSleeve >= 8.13 && shirtSleeve < 8.38)){
-  console.log("S");
-}else if((shirtWidth >= 20 && shirtWidth < 22) && (shirtLength >= 29 && shirtLength < 30) && (shirtSleeve >= 8.38 && shirtSleeve < 8.63)){
-  console.log("M");
-}else if((shirtWidth >= 22 && shirtWidth < 24) && (shirtLength >= 30 && shirtLength < 31) && (shirtSleeve >= 8.63 && shirtSleeve < 8.88)){
-  console.log("L");
-}else if((shirtWidth >= 24 && shirtWidth < 26) && (shirtLength >= 31 && shirtLength < 33) && (shirtSleeve >= 8.88 && shirtSleeve < 9.63)){
-  console.log("XL");
-}else if((shirtWidth >= 26 && shirtWidth < 28) && (shirtLength >= 33 && shirtLength < 34) && (shirtSleeve >= 9.63 && shirtSleeve < 10.13)){
-  console.log("2XL");
-}else if((shirtWidth >= 28) && (shirtLength >= 34) && (shirtSleeve >= 10.13)){
-  console.log("3XL");
-}else{
-  console.log("N/A");
-}
+var category = eatsPlants && eatsAnimals ? "omnivore" : eatsPlants && !eatsAnimals ? "herbivore": !eatsPlants && eatsAnimals ? "carnivore" : undefined ;
+
+
+console.log(category);
