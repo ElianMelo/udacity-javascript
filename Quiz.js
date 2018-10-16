2b1bcdf8d9b811282bdd93f91583ff4280b10354
ElianMelo <elianmelomorais@gmail.com>
2018-10-16 20:56:37 -0300

    Aula 4 - Quiz: 99 garrafas de suco

--- Quiz.js
+++ Quiz.js
@@ -7,20 +7,35 @@ Objetivo: Mostrar os códigos de quiz utilizados no curso da udacity
 */
 
 /*
- * Programming Quiz: JuliaJames (4-1)
+ * Programming Quiz: 99 Bottles of Juice (4-2)
+ *
+ * Use the following `while` loop to write out the song "99 bottles of juice".
+ * Log the your lyrics to the console.
+ *
+ * Note
+ *   - Each line of the lyrics needs to be logged to the same line.
+ *   - The pluralization of the word "bottle" changes from "2 bottles" to "1 bottle" to "0 bottles".
  */
 
-var x = 1;
+var num = 99;
+var str = "bottles";
+var strAux = "bottles";
 
-while (x <= 20) {
-    if(x % 3 === 0 && x % 5 === 0){
-        console.log("JuliaJames");
-    }else if(x % 3 === 0){
-        console.log("Julia");
-    }else if(x % 5 === 0){
-        console.log("James");
-    }else{
-        console.log(x);
-    }
-    x = x + 1;
+while (num >= 1) {
+  var numAux = num - 1;
+
+  if(num === 1){
+    str = "bottle";
+  }else{
+    str = "bottles";
+  }
+
+  if(numAux === 1){
+    strAux = "bottle";
+  }else{
+    strAux = "bottles";
+  }
+
+  console.log(num + " " + str + " of juice on the wall! " + num + " " + str + " of juice! Take one down, pass it around... " + numAux + " " + strAux + " of juice on the wall!");
+  num = num - 1;
 }
