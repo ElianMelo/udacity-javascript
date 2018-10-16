81d490733473edc7143c4e4a4d1a348ca30a6802
ElianMelo <elianmelomorais@gmail.com>
2018-10-16 20:51:15 -0300

    Aula 3 - Quiz: Sorvete

--- Quiz.js
+++ Quiz.js
@@ -7,24 +7,28 @@ Objetivo: Mostrar os códigos de quiz utilizados no curso da udacity
 */
 
 /*
- * Programming Quiz - Checking Your Balance (3-5)
+ * Programming Quiz: Ice Cream (3-6)
+ *
+ * Write a single if statement that logs out the message:
+ *
+ * "I'd like two scoops of __________ ice cream in a __________ with __________."
+ *
+ * ...only if:
+ *   - flavor is "vanilla" or "chocolate"
+ *   - vessel is "cone" or "bowl"
+ *   - toppings is "sprinkles" or "peanuts"
+ *
+ * We're only testing the if statement and your boolean operators.
+ * It's okay if the output string doesn't match exactly.
  */
 
-// change the values of `balance`, `checkBalance`, and `isActive` to test your code
-var balance = 325.00;
-var checkBalance = true;
-var isActive = false;
+// change the values of `flavor`, `vessel`, and `toppings` to test your code
+var flavor = "strawberry";
+var vessel = "cone";
+var toppings = "cookies";
 
-// your code goes here
+// Add your code here
 
-if(checkBalance === false){
-    console.log("Thank you. Have a nice day!");
-}else if(isActive === true && balance > 0){
-    console.log("Your balance is $" + balance + ".");
-}else if(isActive !== true){
-    console.log("Your account is no longer active.");
-}else if(balance === 0){
-    console.log("Your account is empty.");
-}else{
-    console.log("Your balance is negative. Please contact bank.");
+if((flavor == "vanilla" || flavor == "chocolate") && (vessel == "cone" || vessel == "bowl") && (toppings == "sprinkles" || toppings == "peanuts")){
+    console.log("I'd like two scoops of " + flavor + " ice cream in a " + vessel + " with " + toppings + ".");
 }
