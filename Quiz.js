3adeeaf55ccde60767c9747c1dac5e706f8db85e
ElianMelo <elianmelomorais@gmail.com>
2018-10-16 20:56:01 -0300

    Aula 4 - Quiz: JuliaJames

--- Quiz.js
+++ Quiz.js
@@ -7,43 +7,20 @@ Objetivo: Mostrar os códigos de quiz utilizados no curso da udacity
 */
 
 /*
- * Programming Quiz: Back to School (3-9)
- *
- * Write a switch statement to set the average salary of a person based on their type of completed education.
- *
+ * Programming Quiz: JuliaJames (4-1)
  */
 
-// change the value of `education` to test your code
-var education = 'no high school diploma';
+var x = 1;
 
-// set the value of this based on a person's education
-var salary = 0;
-
-// your code goes here
-switch(education){
-    case "no high school diploma":
-        salary = 25636;
-        break;
-    case "a high school diploma":
-        salary = 35256;
-        break;
-    case "an Associate's degree":
-        salary = 41496;
-        break;
-    case "a Bachelor's degree":
-        salary = 59124;
-        break;
-    case "a Master's degree":
-        salary = 69732;
-        break;
-    case "a Professional degree":
-        salary = 89960;
-        break;
-    case "a Doctoral degree":
-        salary = 84396;
-        break;
+while (x <= 20) {
+    if(x % 3 === 0 && x % 5 === 0){
+        console.log("JuliaJames");
+    }else if(x % 3 === 0){
+        console.log("Julia");
+    }else if(x % 5 === 0){
+        console.log("James");
+    }else{
+        console.log(x);
+    }
+    x = x + 1;
 }
-
-salary = salary.toLocaleString("en-US");
-
-console.log("In 2015, a person with " + education + " earned an average of $" + salary + "/year.");
