5c0e3b2ca8904c47c762123ed43f5b1b9a5c30b8
ElianMelo <elianmelomorais@gmail.com>
2018-10-16 20:57:46 -0300

    Aula 4 - Quiz: Modificando o loop

--- Quiz.js
+++ Quiz.js
@@ -7,37 +7,10 @@ Objetivo: Mostrar os códigos de quiz utilizados no curso da udacity
 */
 
 /*
- * Programming Quiz: Countdown, Liftoff! (4-3)
- *
- * Using a while loop, print out the countdown output above.
+ * Programming Quiz: Changing the Loop (4-4)
  */
 
-// your code goes here
-var seconds = 60;
-
-while(seconds >= 0){
-  switch (seconds) {
-    case 50:
-        console.log("Orbiter transfers from ground to internal power");
-        break;
-    case 31:
-        console.log("Ground launch sequencer is go for auto sequence start");
-        break;
-    case 16:
-        console.log("Activate launch pad sound suppression system");
-        break;
-    case 10:
-        console.log("Activate main engine hydrogen burnoff system");
-        break;
-    case 6:
-        console.log("Main engine start");
-        break;
-    case 0:
-        console.log("Solid rocket booster ignition and liftoff!");
-        break;
-    default:
-        console.log("T-" + seconds + " seconds");
-  }
-
-    seconds = seconds - 1;
+// rewrite the while loop as a for loop
+for(var x = 9; x >= 1; x--){
+    console.log("hello " + x);
 }
