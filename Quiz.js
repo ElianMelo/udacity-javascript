266ca51417f4e68e8540274c0fc7a34aaa734f34
ElianMelo <elianmelomorais@gmail.com>
2018-10-16 20:50:42 -0300

    Aula 3 - Quiz: Verificando seu saldo

--- Quiz.js
+++ Quiz.js
@@ -7,30 +7,24 @@ Objetivo: Mostrar os códigos de quiz utilizados no curso da udacity
 */
 
 /*
- * Programming Quiz: Murder Mystery (3-4)
+ * Programming Quiz - Checking Your Balance (3-5)
  */
 
-// change the value of `room` and `suspect` to test your code
-var room = "dining room";
-var suspect = "Mr. Parkes";
+// change the values of `balance`, `checkBalance`, and `isActive` to test your code
+var balance = 325.00;
+var checkBalance = true;
+var isActive = false;
 
-var weapon = "";
-var solved = false;
+// your code goes here
 
-if (room == "ballroom") {
-    weapon = "poison";
-    solved = true;
-} else if (room == "gallery") {
-    weapon = "trophy";
-    solved = true;
-} else if (room == "billiards room") {
-    weapon = "pool stick";
-    solved = true;
-} else {
-    weapon = "knife";
-    solved = true;
-}
-
-if (solved) {
-	console.log(suspect + " did it in the " + room  + " with the " + weapon + "!");
+if(checkBalance === false){
+    console.log("Thank you. Have a nice day!");
+}else if(isActive === true && balance > 0){
+    console.log("Your balance is $" + balance + ".");
+}else if(isActive !== true){
+    console.log("Your account is no longer active.");
+}else if(balance === 0){
+    console.log("Your account is empty.");
+}else{
+    console.log("Your balance is negative. Please contact bank.");
 }
