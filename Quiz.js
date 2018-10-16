4500898db847d525ce543cfec56f2b55d8e50caa
ElianMelo <elianmelomorais@gmail.com>
2018-10-16 20:58:28 -0300

    Aula 4 - Quiz: Conserte o erro 2

--- Quiz.js
+++ Quiz.js
@@ -7,10 +7,10 @@ Objetivo: Mostrar os códigos de quiz utilizados no curso da udacity
 */
 
 /*
- * Programming Quiz: Fix the Error 1 (4-5)
+ * Programming Quiz: Fix the Error 2 (4-6)
  */
 
 // fix the for loop
-for (var x = 5; x < 10; x++) {
-    console.log(x);
+for (var k = 0; k < 200; k++) {
+    console.log(k);
 }
