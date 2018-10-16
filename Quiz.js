74a7b0005d43f98168b073b44cc12a10035f8d9f
ElianMelo <elianmelomorais@gmail.com>
2018-10-16 20:33:44 -0300

    Aula 2 - Quiz: Comida favorita

--- Quiz.js
+++ Quiz.js
@@ -7,18 +7,7 @@ Objetivo: Mostrar os códigos de quiz utilizados no curso da udacity
 */
 
 /*
- * Programming Quiz: Converting Tempatures (2-2)
- *
- * The Celsius-to-Fahrenheit formula:
- *
- *    F = C x 1.8 + 32
- *
- * 1. Set the fahrenheit variable to the correct value using the celsius variable and the forumla above
- * 2. Log the fahrenheit variable to the console
- *
+ * Programming Quiz: Favorite Food (2-3)
  */
 
-var celsius = 12;
-var fahrenheit = celsius * 1.8 + 32;
-
-console.log(fahrenheit);
+console.log("Pamonha");
