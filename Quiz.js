c85b7b6ce66675b1dac56b184b24c0219b390ded
ElianMelo <elianmelomorais@gmail.com>
2018-10-16 20:32:10 -0300

    Aula 2 - Quiz: Primeira expressão

--- Quiz.js
+++ Quiz.js
@@ -5,3 +5,13 @@ Data de Criação: 16/10/2018
 
 Objetivo: Mostrar os códigos de quiz utilizados no curso da udacity
 */
+
+/*
+ * Programming Quiz: First Expression (2-1)
+ *
+ * Write an expression that uses at least three, different, arithmetic operators
+ * to log the number 42 to the console.
+ */
+
+// this expression equals 4, change it to equal 42
+console.log(2 * 42 / 2 - 42 + 42);
