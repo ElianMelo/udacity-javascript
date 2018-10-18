66dfe7a7db7928d7c294ff7887971cdaf5b89751
ElianMelo <elianmelomorais@gmail.com>
2018-10-18 15:31:19 -0300

    Aula 6 - Quiz: Eu tenho contas

--- Quiz.js
+++ Quiz.js
@@ -5,28 +5,35 @@ Objetivo: Mostrar os códigos de quiz utilizados no curso da udacity
 */
 
 /*
- * Programming Quiz: Another Type of Loop (6-8)
+ * Programming Quiz: I Got Bills (6-9)
  *
- * Use the existing `test` variable and write a `forEach` loop
- * that adds 100 to each number that is divisible by 3.
+ * Use the .map() method to take the bills array below and create a second array
+ * of numbers called totals. The totals array should contains each amount from the
+ * bills array but with a 15% tip added. Log the totals array to the console.
+ *
+ * For example, the first two entries in the totals array would be:
+ *
+ * [57.76, 21.99, ... ]
  *
  * Things to note:
- *  - you must use an `if` statement to verify code is divisible by 3
- *  - you can use `console.log` to verify the `test` variable when you're finished looping
+ *  - each entry in the totals array must be a number
+ *  - each number must have an accuracy of two decimal points
  */
 
-var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
-    19, 300, 3775, 299, 36, 209, 148, 169, 299,
-    6, 109, 20, 58, 139, 59, 3, 1, 139
+var bills = [50.23, 19.12, 34.01,
+    100.11, 12.15, 9.90, 29.11, 12.99,
+    10.00, 99.22, 102.20, 100.10, 6.77, 2.22
 ];
 
-// Write your code here
-test.forEach(
-function somaCem(elemento, indice, array) {
-    if(elemento % 3 === 0){
-        array[indice] = elemento + 100;
-    }
+var totals = bills.map(
+
+function gorjeta(totals) {
+    totals += totals*0.15;
+    totals = totals.toFixed(2);
+    totals = Number(totals);
+    return totals;
 }
+
 );
 
-console.log(test);
+console.log(totals);
